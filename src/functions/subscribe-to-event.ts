eb40b84433238baf34f1d0fa68b25ff8456dab00
LaysAguiar <lays.aguiar.dev@gmail.com>
2025-02-22 00:28:27 -0300

    feat: add access invite link functionality and list subscriptions endpoint; update environment variables

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -9,16 +9,24 @@ interface SubscribeToEventParams {
 
 
 export async function subscribeToEvent({ name, email }: SubscribeToEventParams) {
-
     const result = await db.insert(subscriptions).values({
         name,
         email
     }).returning()
-
+    console.log(result)
     const subscriber = result[0]
 
     return {
         subscriberId: subscriber.id,
     }
+
+}
+
+
+export async function listSubscriptions() {
+
+    const result = await db.select().from(subscriptions)
+
+    return result
 
 }
--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,6 +1,6 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
-import z from "zod"
-import { subscribeToEvent } from "../functions/subscribe-to-event"
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
+import { listSubscriptions, subscribeToEvent } from "../functions/subscribe-to-event";
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
 
@@ -8,7 +8,6 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
 
         schema: {
             summary: 'subscribe someone to the event',
-
             body: z.object({
                 name: z.string().min(3).max(255),
                 email: z.string().email(),
@@ -28,8 +27,24 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
         return replay.status(201).send({ subscriberId })
     })
 
-    app.get('/subscriptions', () => {
-        return 'hello word'
+    app.get('/subscriptions', {
+
+        schema: {
+            summary: 'List all Subscriptions',
+            response: {
+
+                200: z.array(z.object({
+                    id: z.string(),
+                    name: z.string(),
+                    email: z.string(),
+                }))
+            }
+
+        }
+    }, async (request, replay) => {
+        const result = await listSubscriptions()
+        return replay.status(200).send(result)
+
     })
 
 }
