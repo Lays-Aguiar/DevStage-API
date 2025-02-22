8ba9c14a32e1d295e8b931ef0b1764298412c1e8
LaysAguiar <lays.aguiar.dev@gmail.com>
2025-02-21 21:50:27 -0300

    add drizzle configuration and subscriptions table migration for PostgreSQL

---
+++ src/drizzle/client.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { env } from "../env";
+import { subscriptions } from './tables/subscripitions';
+
+export const pg = postgres(env.POSTGRES_URL);
+
+export const db = drizzle(pg, {
+
+   schema:{
+         subscriptions
+   } 
+   
+})
+
